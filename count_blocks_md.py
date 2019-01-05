ae7d19650855ea7575decc72b39cf276c8e61b8f
liu1992yang <liu1992yang@gmail.com>
2019-01-04 23:54:31 -0800

    simplify some indent and rearranged variable

--- count_blocks_md.py
+++ count_blocks_md.py
@@ -1,5 +1,4 @@
 import sys
-import re
 
 FILTER = 4
 
@@ -23,9 +22,9 @@ def get_center(block_label):
 
 def read_log_filter(datalog, common):
   print("Reading data file")
+  col_sum_dict = {}
+  listCounts = []
   with open(datalog, "r") as fin:
-    col_sum_dict = {}
-    listCounts = []
     for line in fin:
       if line.startswith(" "):
         continue
@@ -34,20 +33,19 @@ def read_log_filter(datalog, common):
         
         print("Got " + str(len(colNames)) + " columns!")
         continue
-      else :
-        numbers = line.strip().split()
-        if len(numbers) == 0:
-          break
-        dists = list(map(float, numbers))
-        counts = list(map(lambda x: x <= FILTER, dists))
-        listCounts.append(counts)
-    step_contact_bool = list(map(max, listCounts))
-    count_col = list(zip(*listCounts)) #*sign to unpack nested list/tuple
-    sum_col = list(map(sum, count_col))
-    #print(step_contact_bool)
-    for i in range(len(colNames)):
-      col_sum_dict[colNames[i]] = sum_col[i]
-    return col_sum_dict, step_contact_bool
+      strip_line = line.strip()
+      if strip_line == '':
+        break
+      numbers = strip_line.split()
+      counts = list(map(lambda x: float(x) <= FILTER, numbers))
+      listCounts.append(counts)
+  step_contact_bool = list(map(max, listCounts))
+  count_col = list(zip(*listCounts)) #*sign to unpack nested row list/tuple into columns 
+  sum_col = list(map(sum, count_col))
+  #print(step_contact_bool)
+  for i in range(len(colNames)):
+    col_sum_dict[colNames[i]] = sum_col[i]
+  return col_sum_dict, step_contact_bool
 
 
 def total_contact_steps(step_contact_bool):
@@ -82,7 +80,7 @@ def write_out_flat(block_fraction,sum_all_block,contact_steps, output):
 
 if __name__ == '__main__':
   if len(sys.argv) < 4:
-    print("python count_blocks_md.py label_by_block.txt datalog output.txt")
+    print("python countResidue.py label_by_block.txt datalog output.txt")
     sys.exit()
   labelFile = sys.argv[1] 
   datalog = sys.argv[2]
@@ -95,7 +93,7 @@ if __name__ == '__main__':
   block_sum=sum_by_residue(block_label, col_sum_dict) #total cout
   block_frac, sum_all_block = by_ratio(block_sum)
   contact_steps = total_contact_steps(step_contact_bool)
-  #print('contact_steps = {}'.format(str(contact_steps)))
+  print('contact_steps = {}'.format(str(contact_steps)))
   write_out_flat(block_frac, sum_all_block, contact_steps, fout_name) 
 
 
